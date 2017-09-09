d5e41063d9dacd5f7003c174e4109ed120e856ab
Andrew Lam <atlam995@gmail.com>
2017-09-09 16:15:53 -0700

    Added Student objects into array

--- index.php
+++ index.php
@@ -11,8 +11,46 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
-        echo "Hey Whats's Up?";
+        include('Student.php');
+
+        $students = array();
+
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home', 'john@doe.com');
+        $first->add_email('work', 'jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home', 'albert@braniacs.com');
+        $second->add_email('work1', 'a_einstein@bcit.ca');
+        $second->add_email('work2', 'albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
+
+        $third = new Student();
+        $third->surname = "Lam";
+        $third->first_name = "Andrew";
+        $third->add_email('home', 'atlam995@gmail.com');
+        $third->add_email('work1', 'andrewlam95@bcit.ca');
+        $third->add_grade(50);
+        $third->add_grade(50);
+        $third->add_grade(50);
+        $students['a789'] = $third;
+
+        ksort($students); // one of the many sort functions
+
+        foreach ($students as $student) {
+            echo $student->toString();
+        }
         ?>
     </body>
 </html>
